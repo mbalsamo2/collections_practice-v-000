748a8f71b8b6d7009dcdc24f68cf594925d1946c
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-23 02:49:07 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  array.each do |i|
+    i[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select {|i| i.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject(0, :+)
+end
+
+def add_s(array)
+  array.each.collect do |word|
+    if word == "feet"
+      "feet"
+    else
+      word + "s"
+    end
+  end
+end
